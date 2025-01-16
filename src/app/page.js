8d553a9685ed7d3a4eda3200267bdb796d5b06c1
gaurav-wtl <gfullstackwtl@gmail.com>
2025-01-16 16:04:14 +0530

    update invoice after submit

--- src/app/page.js
+++ src/app/page.js
@@ -449,6 +449,7 @@ const CreateInvoice = () => {
           <select
             id="jtype"
             name="jtype"
+            value={data.journeyType}
             onChange={(e) => setData({ ...data, journeyType: e.target.value })}
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           >
